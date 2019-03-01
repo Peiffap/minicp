18d4499a8b6c40a652797d64262012b57573007e
Guillaume Derval <guillaume@guillaumederval.be>
2019-03-01 14:25:10 +0100

    Element1DDomainConsistent

---
+++ src/test/java/minicp/engine/constraints/Element1DDCTest.java
@@ -0,0 +1,30 @@
+/*
+ * mini-cp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License  v3
+ * as published by the Free Software Foundation.
+ *
+ * mini-cp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU Lesser General Public License  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mini-cp. If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
+ *
+ * Copyright (c)  2018. by Laurent Michel, Pierre Schaus, Pascal Van Hentenryck
+ */
+
+package minicp.engine.constraints;
+
+import minicp.engine.SolverTest;
+import minicp.util.NotImplementedExceptionAssume;
+import minicp.util.exception.NotImplementedException;
+import org.junit.Test;
+
+public class Element1DDCTest extends SolverTest {
+
+    @Test
+    public void element1dTest1() {
+        //TODO
+        NotImplementedExceptionAssume.fail(new NotImplementedException());
+    }
+}
