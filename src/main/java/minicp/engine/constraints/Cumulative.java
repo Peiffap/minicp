e23f58f897bba9009514063195d655d75a10cc33
Gilles Peiffer <gilles.peiffer@outlook.com>
2020-04-27 21:33:04 +0200

    TP 9: Add Cumulative constraint

--- src/main/java/minicp/engine/constraints/Cumulative.java
+++ src/main/java/minicp/engine/constraints/Cumulative.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 
 import static minicp.cp.Factory.minus;
 import static minicp.cp.Factory.plus;
+import static minicp.util.exception.InconsistencyException.INCONSISTENCY;
 
 /**
  * Cumulative constraint with time-table filtering
@@ -83,22 +84,72 @@ public class Cumulative extends AbstractConstraint {
     @Override
     public void propagate() {
         Profile profile = buildProfile();
-        // TODO 2: check that the profile is not exceeding the capa otherwise throw an INCONSISTENCY
-        for (int i = 0; i < profile.size(); i++) {
-             throw new NotImplementedException("Cumulative");
+        // 2: check that the profile is not exceeding the capa otherwise throw an INCONSISTENCY
+        Rectangle[] rectangles = profile.rectangles();
+        // Check whether at least one rectangle in the profile exceeds the capacity.
+        for (Rectangle r: rectangles) {
+            if (r.height() > capa) {
+                throw INCONSISTENCY;
+            }
         }
 
         for (int i = 0; i < start.length; i++) {
             if (!start[i].isBound()) {
                 // j is the index of the profile rectangle overlapping t
                 int j = profile.rectangleIndex(start[i].min());
-                // TODO 3: push i to the right
+                // 3: push i to the right
                 // hint:
                 // Check that at every-point on the interval
                 // [start[i].getMin() ... start[i].getMin()+duration[i]-1] there is enough space.
                 // You may have to look-ahead on the next profile rectangle(s)
                 // Be careful that the activity you are currently pushing may have contributed to the profile.
-                 throw new NotImplementedException();
+
+                // Start and end of interval.
+                int first = start[i].min();
+                int last = first + duration[i];
+
+                int max = start[i].max();
+
+                // rect is the profile rectangle overlapping at the current time.
+                Rectangle rect = profile.get(profile.rectangleIndex(first));
+                int rEnd = rect.end();
+                int rHeight = rect.height();
+
+                int removePosition = first - 1; // Position below which to remove.
+
+                // Find the earliest starting time which does not violate the capa constraint.
+                for (int t = first; t < last; t++) {
+                    // Check whether this part of activity i has already been counted at t:
+                    // beenCounted <=> start[i].max = max <= t < last = end[i].min(),
+                    // where the second inequality is guaranteed by the for loop.
+                    // and choose the demand left to contribute:
+                    //  beenCounted => toContribute = 0,
+                    // !beenCounted => toContribute = demand[i].
+                    boolean tGeMax = t >= max;
+                    int toContribute = tGeMax ? 0 : demand[i];
+
+                    // Recompute height at t if needed.
+                    if (rEnd <= t) {
+                        rect = profile.get(profile.rectangleIndex(t));
+                        rEnd = rect.end();
+                        rHeight = rect.height();
+                    }
+
+                    // If the profile at t exceeds the capacity,
+                    // then activity i must start after t.
+                    if (rHeight + toContribute > capa) {
+                        // If we are going to remove everything below t + 1,
+                        // and t >= max, then we can already throw the error.
+                        if (tGeMax) {
+                            throw INCONSISTENCY;
+                        } else {
+                            removePosition = t;
+                        }
+                    }
+                }
+
+                // Remove every start position beyond the last failure.
+                start[i].removeBelow(removePosition + 1);
             }
         }
     }
@@ -106,10 +157,13 @@ public class Cumulative extends AbstractConstraint {
     public Profile buildProfile() {
         ArrayList<Rectangle> mandatoryParts = new ArrayList<Rectangle>();
         for (int i = 0; i < start.length; i++) {
-            // TODO 1: add mandatory part of activity i if any
-             throw new NotImplementedException("Cumulative");
+            // 1: add mandatory part of activity i if any
+            int s = start[i].max();
+            int e = end[i].min();
+            if (s < e) {
+                mandatoryParts.add(new Rectangle(s, e, demand[i]));
+            }
         }
         return new Profile(mandatoryParts.toArray(new Profile.Rectangle[0]));
     }
-
 }
